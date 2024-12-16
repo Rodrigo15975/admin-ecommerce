1de80285345740d83f88a88e5f8d63a26ade4f02
rodrigo <javierluis15975@hotmail.com>
2024-12-16 00:56:42 -0500

    update page.tsx

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,6 +1,5 @@
 import ProductsPage from '@/modules/products/pages/productsPage'
-import type { NextPage } from 'next'
 
-const Page: NextPage = () => <ProductsPage />
+const Page = () => <ProductsPage />
 
 export default Page
